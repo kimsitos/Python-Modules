7002da8e7be777dd26f2b7f1e5406fa7ba83b1a4
Kimsitos <kimsitos15@gmail.com>
2025-12-15 19:39:14 +0100

    xD i had to redo ex7

--- Module-00/ex6/ft_garden_summary.py
+++ Module-00/ex6/ft_garden_summary.py
@@ -1,6 +1,6 @@
 def ft_garden_summary():
     garden_name = input("Enter garden name: ")
     number_plants = int(input("Enter number of plants: "))
-    print("Garden: ", garden_name)
-    print("Plants: ", number_plants)
+    print("Garden:", garden_name)
+    print("Plants:", number_plants)
     print("Status: Growing well!")
