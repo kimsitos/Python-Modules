a4c5ba58cf10d45987eacdd61b15929c440d1786
Stefan Adrian Cozaciuc <stcozaci@c1r9s5.42madrid.com>
2025-12-17 17:48:42 +0100

    help plis

--- Module-01/ex6/ft_garden_analytics.py
+++ Module-01/ex6/ft_garden_analytics.py
@@ -38,53 +38,52 @@ class PrizeFlower(FloweringPlant):
 
 class GardenManager:
     gardens = {}
-    class GardenStats:
-        @classmethod
-        def report(cls):
-            for name in cls.gardens:
-                print(f"\n=== {name}'s Garden Report ===")
-                print("Plants in garden:")
-                for plant in cls.gardens[name]:
-                    plant.get_info()
-            print("\nTotal gardens managed:", cls.__total_gardens)
+    __total_gardens = 0
 
+    class GardenStats:
+        __total_gardens = 0
+        __regular_plants = 0
+        __flowering_plants = 0
+    
+    
     def __init__(self, garden_name):
         self.garden_name = garden_name.capitalize()
         self.plants = []
         GardenManager.__total_gardens += 1
-        GardenManager.gardens[garden_name.capitalize()] = []
+        GardenManager.gardens[self.garden_name] = self.plants
 
     def add_plant(self, name, height, days, colour):
         plant = Plant(name, height, days)
         self.plants.append(plant)
-        GardenManager.gardens[self.garden_name].append(flower)
+        GardenManager.gardens[self.garden_name].append(plant)
+        self.GardenStats.__regular_plants += 1
         print(f"Added {name.capitalize()} to {self.garden_name}'s garden")
-        GardenManager.__regular_plants += 1
 
     def add_flowering_plant(self, name, height, days, colour):
         flower = FloweringPlant(name, height, days, colour)
         self.plants.append(flower)
         GardenManager.gardens[self.garden_name].append(flower)
+        self.GardenStats.__flowering_plants += 1
         print(f"Added {name.capitalize()} to {self.garden_name}'s garden")
-        GardenManager.__flowering_plants += 1
 
-    def grow(self):
-        print(name, "is helping all plants grow...")
+    def grow_all(self):
+        print(f"\n{self.garden_name} is helping all plants grow...")
         for plant in self.plants:
             plant.grow
             print(plant.name, "grew 1cm")
 
-    @classmethod
-    # def create_garden_netwok():
-    #     garde
-    
+    def report(self):
+        print(f"\n=== {self.garden_name}'s Garden Report ===")
+        print("Plants in garden:")
+        for plant in self.plants:
+            plant.get_info()
 
 
 
-print("=== Garden Management System Demo ===")
+print("=== Garden Management System Demo ===\n")
 Amaia = GardenManager("Amaia")
 El_Bosco = GardenManager("El_Bosco")
-# Dennis = GardenManager.create_garden_netwok()
 Amaia.add_flowering_plant("rose", 30, 4, "rose")
 El_Bosco.add_flowering_plant("violet", 30, 4, "violet")
-GardenManager.GardenStats()
+Amaia.report()
+Amaia.grow_all()
