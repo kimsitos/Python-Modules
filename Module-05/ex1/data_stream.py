38799de2198ff26738f9d300f162b4c6288ae826
Stefan Adrian Cozaciuc <stcozaci@c1r8s2.42madrid.com>
2026-01-21 18:32:50 +0100

    minor change. Python is slowly degrading my mind

--- Module-05/ex1/data_stream.py
+++ Module-05/ex1/data_stream.py
@@ -1,36 +1,59 @@
-from typing import Any
+from typing import Dict, Any
 
 
+# Class
 class DataStream:
-    def process_batch(self) -> None:
-        self.hello = "world" 
+    def __init__(self, ID: str) -> Any:
+        self.streamID = ID
+
+    def process_events(self, events: Dict) -> int:
+        self.num_events = 0
+        for _ in events:
+            self.num_events += 1
+        return self.num_events
 
 
 class SensorStream(DataStream):
-    def process_batch(self, sensor: Any) -> None:
-        super().process_batch()
-        self.sensor = sensor
+    def __init__(self, ID: str) -> None:
+        super().__init__(ID)
+        self.type = "Environmental Data"
+
+    def process_events(self, events: Dict) -> int:
+        super().process_events()
 
 
 class TransactionStream(DataStream):
-    def process_batch(self, transaction: Any) -> None:
-        print(transaction)
+    def __init__(self, ID: str) -> None:
+        super().__init__(ID)
+        self.type = "Financial Data"
+
+    def process_events(self, events: Dict) -> int:
+        return self.hello
 
 
 class EventStream(DataStream):
-    def process_batch(self, event: Any) -> None:
-        print(event)
+    def __init__(self, ID: str) -> None:
+        super().__init__(ID)
+        self.type = "System Events"
+
+    def process_events(self, events: Dict) -> int:
+        return self.hello
 
 
 class StreamProcessor():
     def process_batch(self, data: DataStream) -> None:
-        super().process_batch()
+        if isinstance(data, DataStream):
+            print(data.process_events())
+        else:
+            print("lol nothig")
+        return
 
 
+# Test
 streams = [
-    SensorStream(1),
-    TransactionStream(2),
-    EventStream(3)
+    SensorStream("1").process_events({"error": 2, "maybe": 1234}),
+    TransactionStream("2").process_events({"error": 2, "maybe": 1234}),
+    EventStream("3").process_events({"error": 2, "maybe": 1234})
 ]
 
 processor = StreamProcessor()
