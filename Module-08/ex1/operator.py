ce3cab3037fef1c462b6ab707031f4971f17d3d4
Stefan Adrian Cozaciuc <stcozaci@c1r10s4.42madrid.com>
2026-02-12 14:54:25 +0100

    half done, just needed the requirements.txt

--- Module-08/ex1/operator.py
+++ Module-08/ex1/operator.py
@@ -1,11 +1,55 @@
 from importlib import import_module
+import sys
 
-def processing_matrix():
-    df
+
+# ----------------
+# Matrix functions
+# ----------------
+def fetch_matrix_data():
+    print("\nAnalyzing Matrix data...")
+
+    matrix = {
+        "month":
+            ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+             "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        "leaving": np.random.randint(100, size=12)
+    }
+    df = pd.DataFrame(matrix)
+    fd = open("test.txt", "w")
+    print(df, file=fd)
+    fd.close()
+    return df
+
+
+def generate_visualization(df):
+    print("Generating visualization...")
+    month = df.get('month')
+    leaving = df.get('leaving')
+    if month is None or leaving is None:
+        print("Error. Please insert values for month and leaving")
+        exit(1)
+
+    pyplot.plot(month, leaving, marker='o')
+    pyplot.xlabel("Months")
+    pyplot.ylabel("People leaving")
+    pyplot.title("People living Madrid in 20XX")
+
+    output_file = "matrix_analysis.png"
+    pyplot.savefig(output_file)
+    pyplot.close()
+
+    print("Analysis complete!")
+    print(f"Results saved to: {output_file}")
+
 
 print("\nOPERATOR STATUS: Loading programs...")
 print("\nChecking dependencies:")
 
+# ------------
+# Get librarys
+# ------------
+
+"""Panadas imported for analyze data"""
 try:
     pd = import_module('pandas')
     print(f"[OK] {pd.__name__} ({pd.__version__}) - Data manipulation ready")
@@ -13,26 +57,32 @@ except ModuleNotFoundError as e:
     pd = None
     print("[ERROR]", e)
 
-try:
-    req = import_module('requests')
-    print(f"[OK] {req.__name__} ({req.__version__}) - Network access ready")
-except ModuleNotFoundError as e:
-    req = None
-    print("[ERROR]", e)
 
 try:
-    plt = import_module('matplotlib')
-    print(f"[OK] {plt.__name__} ({plt.__version__}) - Visualization ready")
+    np = import_module('numpy')
+    print(f"[OK] {np.__name__} ({np.__version__}) - Network access ready")
 except ModuleNotFoundError as e:
-    plt = None
+    np = None
     print("[ERROR]", e)
 
-if not pd or not req or not plt:
-    print("none")
-    exit(1)
 
-print("Analyzing Matrix data...\n"
-      "Processing 1000 data points...\n"
-      "Generating visualization...\n"
-      "Analysis complete!\n"
-      "Results saved to: matrix\_analysis.png}")
+"""matplotlib used for visualizationof data.
+In this case used to work with pandas"""
+try:
+    pyplot = import_module('matplotlib')
+    print(f"[OK] {pyplot.__name__} ({pyplot.__version__})"
+          f"- Visualization ready")
+    pyplot = import_module('matplotlib.pyplot')
+except ModuleNotFoundError as e:
+    pyplot = None
+    print("[ERROR]", e)
+
+if pd and np and pyplot:
+    generate_visualization(fetch_matrix_data())
+else:
+    print("\n-- With poetry --"
+          "\npoetry install"
+          "\npoetry run python3 operator"
+          "\n\n-- With pip --"
+          "\npip install -r requirements.txt"
+          "\npython3 operator.py")
